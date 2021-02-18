4f3efd13e55f6956c57ec164a38fd624108279a7
Alfian Hakim <79136106+alfianhakim93@users.noreply.github.com>
2021-02-18 11:05:47 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+clear
+echo -e "\e[92m \e[1m"
+echo "============================================"
+echo "           YOUTUBE-DL INSTALLER"
+echo "                By ALFIAN"
+echo "============================================"
+echo "[*] Apa itu Youtube-dl ??"
+echo "[-] Youtube-dl adalah sebuah command line"
+echo "    yang dapat mengunduh video dan audio"
+echo "    dari YouTube dan ratusan situs lainnya."
+echo "[*] Package Yang Dibutuhkan:"
+echo "[1] Pyhton"
+echo "[2] Youtube-dl"
+echo "[3] Ffmpeg"
+echo "============================================"
+echo "[-] Download +- 80MB"
+echo "[-] Jika diberi pilihan (Y/n) pilih Y saja"
+echo "[-] Run Script ytdl.sh Lalu Enter"
+echo "[-] Lama Install Tergantung Kecepatan Inet"
+echo "============================================"
+echo ""
+read -p "Apakah Anda Ingin Menginstall?? (y/n) : " pilih
+echo ""
+
+if [ $pilih == y ] || [ $pilih == Y ]
+then
+
+echo "Updating Default Package"
+pkg -y update
+
+echo "Meminta Akses Storage"
+sleep 2
+echo "Berikan Izin Akses Storage"
+sleep 2
+termux-setup-storage
+sleep 5
+
+echo "Installing Pyhton"
+pkg install -y python
+pip install --upgrade -y pip
+
+echo "Installing Youtube-dl"
+pip install --upgrade youtube-dl
+
+echo "Installing FFmpeg"
+pkg install -y ffmpeg
+
+mkdir -p /data/data/com.termux/files/bin/ytdl
+echo -e 'tema="\e[39m \e[1m"' > /data/data/com.termux/files/bin/ytdl/tema.txt
+
+echo -e '\nPATH="$PATH:/data/data/com.termux/files/home/Termux-YTDL"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+. /data/data/com.termux/files/usr/etc/bash.bashrc
+chmod +x Termux-YTDL/ytdl.sh
+
+echo -e "\e[92m \e[1m"
+echo "Install Selesai !!"
+echo "Gunakan Script Dengan Baik !!"
+
+else
+exit
+fi
